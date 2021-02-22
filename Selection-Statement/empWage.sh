d2343d19130d1859d3e16914f83f5c47d4ac9b77
stutiid <stutichaturvedi12@gmail.com>
2021-02-22 11:36:20 +0530

    Added: Calculating salary of empoyee

--- Selection-Statement/empWage.sh
+++ Selection-Statement/empWage.sh
@@ -4,7 +4,11 @@ empCheck=$((RANDOM%2))
 isPresent=1
 if [ $empCheck -eq $isPresent ]
 	then
-		echo Employee is present
+		empRatePerHr=20;
+		empHrs=8;
+		salary=$(($empRatePerHr * $empHrs));
+		echo Employee is present so salary is $salary
 else 
-	echo Employee is absent
+	
+	echo Employee is absent so salary is 0
 fi
